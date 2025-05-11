77b620b8232d7f65b28669eccca49a7dd635fd39
dbislimi <dbislimi@student.42nice.fr>
2025-05-11 20:36:17 +0200

    refresh

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+set -e
 
-service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+mysqld_safe --datadir=/var/lib/mysql &
 
-exec mysqld_safe
+while ! mysqladmin ping --silent; do
+    echo "waiting for mariadb server..."
+    sleep 1
+done
+
+mysql -u root <<EOSQL
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWD}';
+FLUSH PRIVILEGES;
+EOSQL
+
+wait
